5fb7eec066de151fdf5754ac4cc3b221d63cefd9
Paweł Szymański <pawelfiu@gmail.com>
2024-08-14 11:01:26 +0200

    Rename domain to wallets

--- src/main/java/com/playtomic/tests/wallet/wallets/WalletController.java
+++ src/main/java/com/playtomic/tests/wallet/wallets/WalletController.java
@@ -1,4 +1,4 @@
-package com.playtomic.tests.wallet.api;
+package com.playtomic.tests.wallet.wallets;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
